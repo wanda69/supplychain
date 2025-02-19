095a96ae3048ff640c266a01ff66a49266c222aa
wanda69 <58586943+wanda69@users.noreply.github.com>
2025-02-18 19:51:58 -0800

    Add files via upload

---
+++ ai.c
@@ -0,0 +1,13 @@
+//
+// Created by Lenovo on 2/17/2025.
+// AI
+
+/* *
+The Dispose method is primarily implemented to release unmanaged resources.
+When working with instance members that are IDisposable implementations, it's common to cascade Dispose calls.
+There are other reasons for implementing Dispose, for example, to free memory that was allocated,
+remove an item that was added to a collection, or signal the release of a lock that was acquired.
+ */
+void Dispose(void * ptr) {
+
+}
